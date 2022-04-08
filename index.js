9a14b514188ad1d0bd61526156e1c507e3ee58c0
omar2004712 <o.bahutair@gmail.com>
2022-04-09 00:33:55 +0400

    added a shuffle feature

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const deck = {
     cards: [],
+    isShuffled: false,
     createDeck(){
+        this.isShuffled = false;
         this.cards.splice(0)
         const suits = ['spades', 'diamonds', 'hearts', 'clubs'];
         const values = "A,2,3,4,5,6,7,8,9,10,J,Q,K".split(',');
@@ -8,12 +10,22 @@ const deck = {
             for(suit of suits){
                 this.cards.push({value, suit})
             }
-        }  
+        }
+        console.log("A deck has been created.")
     },
     drawCard(){
-        let card = this.cards[Math.floor(Math.random()*this.cards.length)]
-        this.cards.splice(this.cards.indexOf(card), 1);
+        let card
+        if(this.isShuffled){
+            card = this.cards[Math.floor(Math.random()*this.cards.length)]
+            this.cards.splice(this.cards.indexOf(card), 1);
+        }
+        else{
+            card = this.cards.pop();
+        }
         return card;
+    },
+    shuffle(){
+        this.isShuffled = true;
+        console.log("The deck has been shuffled.")
     }
-    
 }
